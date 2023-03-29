5f7bc3faffcc5e8d485fb25c7691e5ef6f10ce7f
Janno Leenders <janno.leenders@arvato-scs.com>
2023-03-29 15:30:24 +0200

    demo update

--- Demo1.py
+++ Demo1.py
@@ -1,1 +1,8 @@
-print("Dit is een test voor de Python cursus.")
+name = input("What is your name?")
+print(f"Your name is {len(name)} characters long")
+
+Sentence
+article = input("Enter an article?")
+noun = input("Enter a noun?")
+verb = input("Enter a verb?")
+print(f"{article} {noun} {verb}")
